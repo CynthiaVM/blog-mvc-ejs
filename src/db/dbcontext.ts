a60349bc5123918ce82048beff88d7dd1dc0a318
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-11-06 20:11:46 -0300

    Create dbcontext.ts

---
+++ src/db/dbcontext.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+
+export const dbcontext = new DataSource({
+	type: 'mariadb',
+	host: '127.0.0.1',
+	port: 3307,
+	username: 'root',
+	password: '123456',
+	database: 'blog',
+	logging: true,
+	synchronize: true,
+	entities: [__dirname + '/../**/*.entity.{js,ts}'],
+});
