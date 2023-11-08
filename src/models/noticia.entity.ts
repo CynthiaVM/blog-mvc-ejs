2f364375f68373ea433cf5028e97341e2f172765
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-11-08 20:38:51 -0300

    Create noticia.entity.ts

---
+++ src/models/noticia.entity.ts
@@ -0,0 +1,37 @@
+import { Column,
+	CreateDateColumn, 
+	Entity, 
+	PrimaryGeneratedColumn, 
+	UpdateDateColumn, 
+	OneToMany, 
+	ManyToOne } from 'typeorm';
+
+import { Comentario } from '../models/comentario.entity';
+import { usuarios } from '../models/usuario.entity';
+
+@Entity() //decorador agregando detalles
+export class Noticia { //propiedad que va a tener la noticia
+	@PrimaryGeneratedColumn('uuid') //dice que sera una primarykey (identificador unico)
+	id?: string; //el signo de pregunta es por que puede o no venir
+
+	@Column() //decorador
+	titulo: string;
+
+	@Column()
+	contenido: string;
+
+	@CreateDateColumn()
+	create_at: Date;
+
+    @UpdateDateColumn()
+    update_at: Date;
+    comentarios: any;
+    usuario: any;
+
+	//@OneToMany(() => Comentario, (c) => c.noticia)
+	//comentarios: Comentario[];  //sino lo tengo no podria tener los comentarios 
+
+    //@ManyToOne(() => usuarios, (u) => u.noticias, { nullable: false })
+    //usuario: usuarios;
+
+}
