a213fafe07c5f460d7b65b4acba26dc338a36117
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-11-08 20:38:54 -0300

    Create usuario.entity.ts

---
+++ src/models/usuario.entity.ts
@@ -0,0 +1,45 @@
+import { Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn, 
+	OneToMany,
+	BeforeInsert
+	 } from 'typeorm';
+import bcrypt from 'bcrypt';
+import { Noticia } from '../models/noticia.entity';
+import { before } from 'node:test';
+
+@Entity() //decorador agregando detalles
+export class usuarios { //propiedad que va a tener 
+	@PrimaryGeneratedColumn('uuid') //dice que sera una primarykey (identificador unico)
+	id?: string; //el signo de pregunta es por que puede o no venir
+
+	@Column({ unique: true }) //unico
+	email: string;
+
+	@Column()
+	pass: string;
+
+	@Column()
+	nombre: string;
+
+	@Column()
+	apellido: string;
+
+	@CreateDateColumn()
+	create_at: Date;
+
+	@UpdateDateColumn()
+	updated_at: Date;
+
+	@OneToMany(() => Noticia, (n) => n.usuario)
+	noticias: Noticia[];
+
+	@BeforeInsert()
+	async hashPassword() {
+		this.pass = await bcrypt.hash(this.pass, 10);
+	}
+	
+
+}
