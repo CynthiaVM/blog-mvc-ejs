b717066ce3f1aaf0970526ab8cddba4f4d2f59b8
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-11-08 20:38:43 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,15 +1,14 @@
+
+import 'dotenv/config';
 import express from 'express';
-import dotenv from 'dotenv';
 import path from 'path';
-//import "reflect-metadata";
 import { dbcontext } from './src/db/dbcontext';
 import { TypeORMError } from 'typeorm';
 import logger from './src/helpers/logger';
+import noticiaRoutes from './src/routes/noticia.routes';
 
-
-dotenv.config();
+//dotenv.config();
 const app = express();
-
 
 dbcontext
 .initialize()
@@ -25,14 +24,16 @@ app.set('views', path.join(__dirname, 'src/views'));
 
 const port = process.env.PORT || 2000;
 
-app.get('/', (req, res) => {
-	res.redirect('/ejs');
-});
+app.use('/noticias', noticiaRoutes);
 
-app.get('/ejs', (req, res) => {
-	const nombre = 'Cintia 👌';
-	res.render('home/index', { nombre });
-});
+//app.get('/', (req, res) => {
+//	res.redirect('/ejs');
+//});
+
+//app.get('/ejs', (req, res) => {
+	//const nombre = 'Cintia 👌';
+	//res.render('home/index', { nombre });
+//});
 
 app.listen(port, () => {
 	console.log(`Servidor Express funcionando en http://localhost:${port} 🌈​✅`);
