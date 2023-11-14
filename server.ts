c7f69689a2001975b6a8948dc8f44f4306717891
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-11-13 21:43:05 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -6,34 +6,51 @@ import { dbcontext } from './src/db/dbcontext';
 import { TypeORMError } from 'typeorm';
 import logger from './src/helpers/logger';
 import noticiaRoutes from './src/routes/noticia.routes';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
-//dotenv.config();
 const app = express();
-
+const port = process.env.PORT || 2000;
 dbcontext
-.initialize()
-.then(() => {})
-.catch((err: TypeORMError) => {
-	logger.error(`Error al iniciar la base de datos: ${err.message}`); // si hay error
-});
+	.initialize()
+	.then(() => {})
+	.catch((err: TypeORMError) => {
+		logger.error(`Error al iniciar la base de datos: ${err.message}`); // si hay error
+	});
+
+// Middleware para analizar cookies
+app.use(cookieParser());
+
+// Middleware para gestionar sesiones
+app.use(
+	session({
+		secret: 'my-secret-key',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: false }, // Configuración de la cookie (ajústala según tus necesidades)
+	})
+);
 
 //Configurar EJS
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
-const port = process.env.PORT || 2000;
-
 app.use('/noticias', noticiaRoutes);
 
-//app.get('/', (req, res) => {
-//	res.redirect('/ejs');
-//});
+// Ruta de inicio de sesión
+app.get('/login', (req, res) => {
+	if (req.session) {
+		req.session.user = { id: 1, username: 'usuarioEjemplo' };
+	}
 
-//app.get('/ejs', (req, res) => {
-	//const nombre = 'Cintia 👌';
-	//res.render('home/index', { nombre });
-//});
+	res.send('Inicio de sesión exitoso.');
+});
+
+app.get('/t', (req, res) => {
+	if (req.session) {
+		res.send(req.session.user);
+	}
+});
 
 app.listen(port, () => {
 	console.log(`Servidor Express funcionando en http://localhost:${port} 🌈​✅`);
