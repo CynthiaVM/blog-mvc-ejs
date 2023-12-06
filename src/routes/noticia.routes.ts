dca22212b838310830bd679ed472aa31ad87aed1
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-12-06 02:16:07 -0300

    Update noticia.routes.ts

--- src/routes/noticia.routes.ts
+++ src/routes/noticia.routes.ts
@@ -3,10 +3,12 @@ import {
 	noticiasIndex,
 	crearNoticiaView,
 	crearNoticia,
+	mostrarDetalleNoticia,
 } from '../controllers/noticias.controller';
 
 const noticiasRoutes = express.Router();
 
+noticiasRoutes.get('/detalle/:id', mostrarDetalleNoticia);
 noticiasRoutes.get('/', noticiasIndex);
 noticiasRoutes.get('/crear', crearNoticiaView);
 noticiasRoutes.post('/crear', crearNoticia);
