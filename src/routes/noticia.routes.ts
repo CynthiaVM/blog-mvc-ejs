93ad5084e9aa2c68c0f9179b77fd52cbaa7fd74c
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-12-18 04:31:48 -0300

    Update noticia.routes.ts

--- src/routes/noticia.routes.ts
+++ src/routes/noticia.routes.ts
@@ -1,22 +1,44 @@
-import express from 'express';
+import express, { Router } from 'express';
+import multer from 'multer';  
 import {
-	noticiasIndex,
-	crearNoticiaView,
-	crearNoticia,
-	mostrarDetalleNoticia,
+  noticiasIndex,
+  crearNoticiaView,
+  crearNoticia,
+  getNoticiaById,
+  editarNoticiaView,
+  editarNoticia,
+  listadoNoticias,
+  borrarNoticia,
 } from '../controllers/noticias.controller';
 
 const noticiasRoutes = express.Router();
 
-noticiasRoutes.get('/', noticiasIndex);
-noticiasRoutes.get('/detalle/:id', mostrarDetalleNoticia);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
+const upload = multer({ storage: storage });
+
+noticiasRoutes.get('/', noticiasIndex);
+noticiasRoutes.get('/by/:idNoticia', getNoticiaById);
+
+// Listado
+noticiasRoutes.get('/listado', listadoNoticias);
 //crear
 noticiasRoutes.get('/crear', crearNoticiaView);
-noticiasRoutes.post('/crear', crearNoticia);
+noticiasRoutes.post('/crear', upload.single('imagen'), crearNoticia); // Utilizo 'upload' aca
 //editar
-//noticiasRoutes.get('/editar/:idNoticia', editarNoticiaView);
-//noticiasRoutes.get('/editar/:idNoticia', editarNoticia);
-
+noticiasRoutes.get('/editar/:idNoticia', editarNoticiaView);
+noticiasRoutes.get('/editar/:idNoticia', editarNoticia);
+//delete
+noticiasRoutes.get('/borrar/:idNoticia', borrarNoticia);
 
 export default noticiasRoutes;
+
+//export default noticiasRoutes;
+//export default router;
