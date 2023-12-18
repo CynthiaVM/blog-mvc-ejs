a218d162c5da2db35da1d301b884dc626ef61831
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-12-18 04:31:25 -0300

    Update noticias.controller.ts

--- src/controllers/noticias.controller.ts
+++ src/controllers/noticias.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express';
 import logger from '../helpers/logger';
-import { Inoticias_create } from '../interfaces/noticias/noticias.interfaces';
+import {
+	Inoticias_create,
+	Inoticias_update,
+} from '../interfaces/noticias/noticias.interfaces';
 import { ILike, IsNull } from 'typeorm';
 import { Noticia } from '../models/noticia.entity';
 import { dbcontext } from '../db/dbcontext';
 
 
 export const noticiasIndex = async (req: Request, res: Response) => {
-    console.log('Llegó a noticiasIndex');
+   // console.log('Llegó a noticiasIndex');
     // Aca devolver todas las noticias ordenadas por fecha
 	// ver documentacion de typeorm
     const noticiaRepository = dbcontext.getRepository(Noticia);
@@ -15,6 +18,7 @@ export const noticiasIndex = async (req: Request, res: Response) => {
 	const noticias = await noticiaRepository.find({
 		order: { create_at: 'DESC' },
 		take: 10,
+        where: { deleted_at: IsNull() },
 	});
 
 	res.render('home/index_view_noticias', { noticias });
@@ -22,13 +26,13 @@ export const noticiasIndex = async (req: Request, res: Response) => {
 };
 
 export const crearNoticiaView = (req: Request, res: Response) => {
-    console.log('Llegó a crearNoticiaView');
+    //console.log('Llegó a crearNoticiaView');
     res.render('noticias/crear');
 };
 //async
 export const crearNoticia = async (req: Request, res: Response) => {
-    console.log('Llegó a crearNoticia');
-    console.log('Datos recibidos:', req.body);
+    //console.log('Llegó a crearNoticia');
+    //console.log('Datos recibidos:', req.body);
     const data: Inoticias_create = req.body;
 
 	try {
@@ -49,38 +53,95 @@ export const crearNoticia = async (req: Request, res: Response) => {
 	}
 };
 
-//export const cargarNoticias = async (req: Request, res: Response) => {
-    //try {
-       // const titulo = req.query.titulo_noticia?.toString();
-       // const contenido = req.query.desc_noticia?.toString();
-       // const idNoticia = req.query.id?.toString();
-       // const noticiaRepository = await dbcontext.getRepository(Noticia);
-    
-       // const noticia = await noticiaRepository.find({
-       //     order: {
-         //       create_at: 'DESC',
-         //       },
-        //        take: 10,
-          //  });
-       
-         //   res.render('home/index_view_noticias', { noticia, total: noticia.length});
-     //} catch (error) {
-    //        console.log(error);
-     //   }
-    //};
-    
-    export const obtenerNoticiaPorId = async (id: string) => {
-        const noticiaRepository = await dbcontext.getRepository(Noticia);
-        const noticia = await noticiaRepository.findOne({ where: { id } });
-        return noticia;
-        //where: { id } le dice a TypeORM que busque una noticia donde el campo id coincida con el valor proporcionado.
-    };
+//export const getNoticiaById = async (req: Request, res: Response) => {
+	//console.log(req.params);
+	//const noticiaRepository = dbcontext.getRepository(Noticia);
+	//const noticia = await noticiaRepository.findOneBy({
+	//	id: req.params.idNoticia,
+	//});
+	//res.send({ noticia });
+//};
+export const getNoticiaById = async (req: Request, res: Response) => {
+    try {
+        console.log('Params:', req.params);
+        
+        const noticiaRepository = dbcontext.getRepository(Noticia);
+        const noticia = await noticiaRepository.findOne({
+            where: { id: req.params.idNoticia },
+        });
 
-    export const mostrarDetalleNoticia = async (req: Request, res: Response) => {
-        // Lógica para obtener la noticia individual según el ID y enviarla a la vista
-        const idNoticia = req.params.id;
-        const noticia = await obtenerNoticiaPorId(idNoticia);
-        res.render('noticias/detalle', { noticia });
-    };       
-   
+        console.log('Noticia:', noticia);
 
+        if (!noticia) {
+            console.log('Noticia no encontrada');
+            res.render('shared/error');
+        } else {
+            console.log('Renderizando detalle:', noticia);
+            res.render('noticias/detalle', { noticia });
+        }
+    } catch (error) {
+        console.error('Error en getNoticiaById:', error);
+        res.render('shared/error');
+    }
+};
+
+
+export const editarNoticia = async (req: Request, res: Response) => {
+	try {
+		const noticiaRepository = dbcontext.getRepository(Noticia);
+		const noticia = await noticiaRepository.exist({
+			where: { id: req.params.idNoticia },
+		});
+		if (!noticia) {
+			res.render('shared/error');
+		}
+		const editNoticia: Inoticias_update = {
+			titulo: req.body.titulo,
+			contenido: req.body.contenido,
+		};
+		await noticiaRepository.update(req.params.idNoticia, editNoticia);
+        res.redirect('/noticias/listado');
+
+	} catch (error) {
+		res.render('shared/error');
+	}
+};
+
+export const editarNoticiaView = async (req: Request, res: Response) => {
+	try {
+		const noticiaRepository = dbcontext.getRepository(Noticia);
+		const noticia = await noticiaRepository.findOne({
+			where: { id: req.params.idNoticia },
+		});
+		if (!noticia) {
+			res.render('shared/error');
+		}
+
+		res.render('noticias/editar', { noticia });
+	} catch (error) {
+		res.render('shared/error');
+	}
+};
+
+export const listadoNoticias = async (req: Request, res: Response) => {
+	const noticiaRepository = dbcontext.getRepository(Noticia);
+
+	const noticias = await noticiaRepository.find({
+		order: { create_at: 'DESC' },
+		withDeleted: true,
+	});
+
+	res.render('noticias/listado', { noticias });
+};
+
+export const borrarNoticia = async (req: Request, res: Response) => {
+	const noticiaRepository = dbcontext.getRepository(Noticia);
+	const noticia = await noticiaRepository.findOne({
+		where: { id: req.params.idNoticia },
+	});
+	if (!noticia) {
+		res.render('shared/error');
+	}
+	await noticiaRepository.softDelete(req.params.idNoticia);
+	res.redirect('/noticias/listado');
+};
