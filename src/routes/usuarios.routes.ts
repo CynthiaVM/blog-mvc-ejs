2d61e700576074c55cc42725135f89ccf6637162
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-12-26 01:32:11 -0300

    Update usuarios.routes.ts

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -1,9 +1,25 @@
 import express from 'express';
-import { mostrarFormularioRegistro, registrarUsuario } from '../controllers/usuarios.controller';
+import { 
+  listadoUsuarios,
+  crearUsuarioView,
+  crearUsuario,
+  editarUsuario,
+  editarUsuarioView,
+  eliminarUsuario,
+  recuperarUsuario,
+} from '../controllers/usuarios.controller';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
-const userRoutes = express.Router();
+const usuariosRoutes = express.Router();
 
-userRoutes.get('/registro', mostrarFormularioRegistro);
-userRoutes.post('/registro', registrarUsuario);
+usuariosRoutes.get('/listado', authMiddleware, listadoUsuarios);
+//usuariosRoutes.get('/registro', mostrarFormularioRegistro);
+//usuariosRoutes.post('/registro', registrarUsuario);
+usuariosRoutes.get('/crear', authMiddleware, crearUsuarioView);
+usuariosRoutes.post('/crear', authMiddleware, crearUsuario);
+usuariosRoutes.get('/editar/:idUsuario', authMiddleware, editarUsuarioView);
+usuariosRoutes.post('/editar/:idUsuario', authMiddleware, editarUsuario);
+usuariosRoutes.get('/eliminar/:idUsuario', authMiddleware, eliminarUsuario);
+usuariosRoutes.get('/recuperar/:idUsuario', authMiddleware, recuperarUsuario);
 
-export default userRoutes;
+export default usuariosRoutes;
