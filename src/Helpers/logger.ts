c8e5f6f47d3650b7bb70724fe7ca447ba607abeb
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-12-26 01:31:44 -0300

    Update logger.ts

--- src/Helpers/logger.ts
+++ src/Helpers/logger.ts
@@ -1,15 +1,12 @@
-import { createLogger, 
-	format, 
-	transports } from 'winston';
+import { createLogger, format, transports } from 'winston';
 import dotenv from 'dotenv';
 dotenv.config();
-
 
 const logFormat = format.combine(
 	format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), //formato de tiempo
 	format.printf(({ timestamp, level, message }) => {
 		return `${timestamp} [${level}]: ${message}`;
-	}) //configuracion del formato del blog 
+	}) //configuracion del formato del blog
 );
 const logFormatConsole = format.combine(
 	format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
@@ -24,10 +21,12 @@ const logger = createLogger({
 	format: logFormat,
 	transports: [
 		new transports.Console(),
-		new transports.File({ format: logFormat,
+		new transports.File({
+			format: logFormat,
 			filename: 'app.log',
 			maxFiles: 2,
-			maxsize: 1024, }),
+			maxsize: 1024,
+		}),
 	], // el transports es donde quiero que lo registre
 });
 
