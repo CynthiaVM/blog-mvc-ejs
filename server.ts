17c6bc312a6b31fba144d9c17f279664ca9599d8
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-12-26 01:31:29 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,23 +1,21 @@
-import express, { Request, Response } from 'express';
 import 'dotenv/config';
+import express, { Request, Response } from 'express';
 import path from 'path';
 import { dbcontext } from './src/db/dbcontext';
 import { TypeORMError } from 'typeorm';
 import logger from './src/helpers/logger';
+import noticiasRoutes from './src/routes/noticia.routes';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import authRoutes from './src/routes/auth.routes';
 import expressLayouts from 'express-ejs-layouts';
-import noticiaRoutes from './src/routes/noticia.routes';
-import usuarioRoutes from './src/routes/usuarios.routes';
-import { Usuarios } from "./src/models/usuario.entity";
+import { noticiasIndex } from './src/controllers/noticias.controller';
+import usuariosRoutes from './src/routes/usuarios.routes';
+
 const app = express();
-//para recibir la info de crear
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 dbcontext
 	.initialize()
@@ -26,35 +24,39 @@ dbcontext
 		logger.error(`Error al iniciar la base de datos: ${err.message}`); // si hay error
 	});
 
-// configuracion de cookie y sessiones. 
-//Middleware para analizar cookies: procesa la solicitud y se lleva la cookie para poder consultar
+// configuracion de cookie y sessiones
 app.use(cookieParser());
 app.use(
 	session({
-		secret: 'acatienequeirmisecreto',//contraseña
-		resave: false,//en falso no guarda
+		secret: 'acatienequeirmisecreto',
+		resave: false,
 		saveUninitialized: true,
-		cookie: { secure: false },// Configuración de la cookie. False para q la tome en htp
+		cookie: { secure: false },
 	})
 );
 
 //Configurar EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src/views'));
-//EJS layouts
 app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('layout', path.join(__dirname, 'src/views/shared/layout'));
-//Rutas
-app.use('/noticias', noticiaRoutes);
-app.use('/usuarios', usuarioRoutes);
+
+const port = process.env.PORT || 2000;
+
+app.use('/noticias', noticiasRoutes);
+app.use('/usuarios', usuariosRoutes);
 app.use('/auth', authRoutes);
+
+// static files, para ver la imagen
+app.use('/public', express.static('public'));
+
+// default route
 app.use('/', (req: Request, res: Response) => {
 	res.redirect('/noticias');
 });
 
 
 
-const port = process.env.PORT || 2000;
 app.listen(port, () => {
 	console.log(`Servidor Express funcionando en http://localhost:${port} 🌈​✅`);
 });
