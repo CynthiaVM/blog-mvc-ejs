e10507088771320b404ca4e5e6de54fa4a5aafac
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-12-26 01:31:36 -0300

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,58 +1,64 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { Usuarios } from '../models/usuario.entity';
+import bcrypt from 'bcrypt';
+import { dbcontext } from '../db/dbcontext';
+import { Usuarios } from '../models/usuarios.entity';
+import { ErrorDescription } from 'typeorm';
 
-export const mostrarFormularioRegistro = (req: Request, res: Response) => {
-  res.render('registro'); // Renderiza el formulario de registro
-};
 
-export const registrarUsuario = async (req: Request, res: Response) => {
-  try {
-    const { nombre, apellido, email, pass } = req.body;
-
-    // Verificar si el usuario ya existe en la base de datos
-    const usuarioRepository = getRepository(Usuarios);
-    const usuarioExistente = await usuarioRepository.findOne({ where: { email } });
-
-    if (usuarioExistente) {
-      return res.status(400).send('El usuario ya existe');
-    }
-
-    // Crear un nuevo usuario
-    const nuevoUsuario = usuarioRepository.create({
-      nombre,
-      apellido,
-      email,
-      pass, // La contraseña se hasheará en el hook BeforeInsert del modelo
-    });
-
-    // Guardar el nuevo usuario en la base de datos
-    await usuarioRepository.save(nuevoUsuario);
-
-    // Redirige al usuario después de registrarse
-    res.redirect('/login');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error en el registro');
-  }
-};
-  
-export const loginController = (req: Request, res: Response) => {
-	if (req.session) {
-		//logica de login para el usuario
-		req.session.user = { id: 1, username: 'Cynthia' };
-		res.send('Usuario logueado correctamente!');
+//export const mostrarFormularioRegistro = (req: Request, res: Response) => {
+ //// res.render('registro'); // Renderiza el formulario de registro
+//};export const registrarUsuario = async (req: Request, res: Response) => {try {
+    // Lógica de registro aquí si es necesario (puede estar vacía si no hay lógica específica aquí)
+//} catch (error) {
+    //console.error(error);
+   // res.status(500).send('Error en el registro');
+  //}
+//};
+export const loginController = async (req: Request, res: Response) => {
+	try {
+		const usuarioRepository = await dbcontext.getRepository(Usuarios);
+		const usuario = await usuarioRepository.findOneBy({
+			email: req.body.email,
+		});
+		if (usuario) {
+			const passCorrecto = await bcrypt.compare(req.body.pass, usuario.pass);
+			if (passCorrecto && req.session) {
+				req.session.user = {
+					id: usuario.id,
+					email: usuario.email,
+					nombreCompleto: `${usuario.nombre} ${usuario.apellido}`,
+				};
+				res.redirect('/noticias');
+			} else {
+				throw new Error('Usuario/Password Incorrecto');
+			}
+		} else {
+			throw new Error('Usuario/Password Incorrecto');
+		}
+	} catch (error) {
+		console.log(error);
+		res.render('shared/error', { msgError: error });
 	}
+
+	// existe el email ?
+	// si existe
+	// verifico password
 };
+
+export const loginControllerView = (req: Request, res: Response) => {
+	res.render('auth/login', { layout: false });
+};
+
 export const quiensoyController = (req: Request, res: Response) => {
 	if (req.session?.user) {
 		const user = req.session.user;
 
-		res.render('quiensoy', { user });
+		res.send({ user });
 	} else {
 		res.send('No estas logueado');
 	}
 };
+
 export const logoutController = (req: Request, res: Response) => {
 	req.session?.destroy((err) => {});
 	res.send('Session cerrada correctamente');
